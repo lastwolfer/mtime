0ad9994311790e1f6b79055c22fba9e9ecf76d7e
lastwolfer <liuguoyu600@yahoo.com>
2019-11-28 19:28:36 +0800

    m

---
+++ guns-gateway/src/main/java/com/stylefeng/guns/user/modular/user/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.stylefeng.guns.user.modular.user.controller;
+
+public class UserController {
+}
--- guns-user/src/test/java/com/stylefeng/guns/generator/EntityGenerator.java
+++ guns-user/src/test/java/com/stylefeng/guns/generator/EntityGenerator.java
@@ -29,7 +29,7 @@ public class EntityGenerator {
 
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
-        gc.setOutputDir("D:\\tmp\\guns\\guns-rest\\src\\main\\java");//这里写你自己的java目录
+        gc.setOutputDir("D:\\Develop\\Coding WorkingSpace\\git\\guns\\guns-user\\src\\main\\java");//这里写你自己的java目录
         gc.setFileOverride(true);//是否覆盖
         gc.setActiveRecord(true);
         gc.setEnableCache(false);// XML 二级缓存
@@ -64,11 +64,11 @@ public class EntityGenerator {
         // 包配置
         PackageConfig pc = new PackageConfig();
         pc.setParent(null);
-        pc.setEntity("com.stylefeng.guns.rest.persistence.model");
-        pc.setMapper("com.stylefeng.guns.rest.persistence.dao");
-        pc.setXml("com.stylefeng.guns.rest.persistence.dao.mapping");
+        pc.setEntity("com.stylefeng.guns.user.common.persistence.model");
+        pc.setMapper("com.stylefeng.guns.user.common.persistence.dao");
+        pc.setXml("com.stylefeng.guns.user.common.persistence.dao.mapping");
+        pc.setServiceImpl("com.stylefeng.guns.user.common.persistence.service.impl");
         pc.setService("TTT");       //本项目没用，生成之后删掉
-        pc.setServiceImpl("TTT");   //本项目没用，生成之后删掉
         pc.setController("TTT");    //本项目没用，生成之后删掉
         mpg.setPackageInfo(pc);
 
