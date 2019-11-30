953ca2caa7a5153bb15caa0f3b7fe9df297004e1
lastwolfer <liuguoyu600@yahoo.com>
2019-11-30 17:32:44 +0800

    pandax

---
+++ guns-order/src/main/java/com/stylefeng/guns/rest/common/persistence/service/MoocOrderTServiceImpl.java
@@ -0,0 +1,20 @@
+package com.stylefeng.guns.rest.common.persistence.service;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocOrderT;
+import com.stylefeng.guns.rest.common.persistence.dao.MoocOrderTMapper;
+import TTT.IMoocOrderTService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 订单信息表 服务实现类
+ * </p>
+ *
+ * @author Mainzer
+ * @since 2019-11-30
+ */
+@Service
+public class MoocOrderTServiceImpl extends ServiceImpl<MoocOrderTMapper, MoocOrderT> implements IMoocOrderTService {
+
+}
