b50554d1f34d0b023cea9ff661454c710267870e
aganggggg <983901724@qq.com>
2019-12-02 17:42:13 +0800

    order v0.2.1

--- guns-api/src/main/java/com/stylefeng/guns/service/order/vo/OrderVo.java
+++ guns-api/src/main/java/com/stylefeng/guns/service/order/vo/OrderVo.java
@@ -1,17 +1,21 @@
 package com.stylefeng.guns.service.order.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 public class OrderVo implements Serializable {
     private String orderId;
     private String filmName;
+//    @JsonFormat(pattern="yyyy-MM-dd",timezone = "GMT+8")
     private String fieldTime;
     private String cinemaName;
     private String seatsName;
     private String orderPrice;
+//    @JsonFormat(pattern="yyyy-MM-dd",timezone = "GMT+8")
     private String orderTimestamp;
     private String orderStatus;
 }
