05dd59c92f7e4cc5955ccc06e26c22a2fd90f590
lastwolfer <liuguoyu600@yahoo.com>
2019-12-02 15:14:11 +0800

    pandax

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/order/controller/OrderController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/order/controller/OrderController.java
@@ -1,13 +1,11 @@
 package com.stylefeng.guns.rest.modular.order.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-<<<<<<< HEAD
 import com.stylefeng.guns.rest.modular.auth.util.JwtTokenUtil;
 import com.stylefeng.guns.service.order.MoocOrderTService;
 import com.stylefeng.guns.service.order.vo.BaseVo;
 import com.stylefeng.guns.service.order.vo.OrderVo;
 import org.springframework.beans.factory.annotation.Autowired;
-=======
 import com.stylefeng.guns.service.cinema.CinemaService;
 import com.stylefeng.guns.service.film.vo.BaseRespVo;
 import com.stylefeng.guns.service.order.OrderService;
@@ -15,36 +13,19 @@ import com.stylefeng.guns.service.order.vo.OrderVo;
 import com.stylefeng.guns.service.user.beans.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
->>>>>>> 942826bed87baf1fbc9423ebe3f52fe4bf59e6e9
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-<<<<<<< HEAD
-
-=======
->>>>>>> 942826bed87baf1fbc9423ebe3f52fe4bf59e6e9
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("order")
 public class OrderController {
 
-<<<<<<< HEAD
-    @Reference(interfaceClass = MoocOrderTService.class)
-    MoocOrderTService moocOrderTService;
     @Autowired
     JwtTokenUtil jwtTokenUtil;
 
-    @RequestMapping("buyTickets")
-    public BaseVo buyTickets(Integer fieldId,String soldSeats,String seatsName){
-//        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-//        String authorization = request.getHeader("Authorization");
-//        String authToken = authorization.substring(7);
-//        String userName = jwtTokenUtil.getUsernameFromToken(authToken);
-//        OrderVo orderVo = moocOrderTService.buyTickets(fieldId,soldSeats,seatsName,userName);
-        return null;
-=======
     @Reference(interfaceClass = CinemaService.class, check = false)
     CinemaService cinemaService;
 
@@ -95,7 +76,6 @@ public class OrderController {
         String authorization = request.getHeader("Authorization");
         String authToken = authorization.substring(7);
         return (UserInfo) redisTemplate.opsForValue().get(authorization);
->>>>>>> 942826bed87baf1fbc9423ebe3f52fe4bf59e6e9
     }
 
 }
